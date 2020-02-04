6f10acafdc4bbd13ef94d28bec79f84bf9934efc
Garrett Reichman <gsreichman@gmail.com>
2020-02-03 21:29:02 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+const page = require("./page.js");
+
+
+
+
+page.createPage("hi");
---
+++ page.js
@@ -0,0 +1,13 @@
+const createPage = info=>{
+    console.log(info);
+}
+
+
+
+
+
+
+
+module.exports={
+    createPage : createPage
+}
